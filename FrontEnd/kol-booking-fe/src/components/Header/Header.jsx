59a1c2ba8bcdc16d643755071e3d9764b90213b3
VietNT2011 <ntv142018@gmail.com>
2025-11-16 14:14:58 +0700

    Initial project setup

---
+++ FrontEnd/kol-booking-fe/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Header classname='header'>
+        <div className="header__logo">KOL Booking</div>
+        <div></div>
+    </Header>
+  )
+}
+
+export default Header
