eaa43de69e43e6bca80e0bdefe295fc3efa14c5b
VietNT2011 <ntv142018@gmail.com>
2025-11-20 10:39:30 +0700

    add section to homepage

--- FrontEnd/kol-booking-fe/src/pages/Home/Home.jsx
+++ FrontEnd/kol-booking-fe/src/pages/Home/Home.jsx
@@ -4,7 +4,8 @@ import tvc from '../../assets/hero-tvc.mov'
 import img1 from '../../assets/img_solution_1.png'
 import img2 from '../../assets/img_solution_2.png'
 import img3 from '../../assets/img_solution_3.jpg'
-import mission_first from '../../assets/missionimage/mission_first.png'
+import vision_image from '../../assets/visionimage/visionimage.png'
+import strength_image from '../../assets/strengthimage/strength_img.png'
 import './Home.css'
 import BrandSlider from "../../components/BrandSlider/BrandSlider";
 const Home = () => {
@@ -61,24 +62,51 @@ const Home = () => {
                 
             </div>
 
-            <BrandSlider />
+        </div>
 
-            <div className="home__vision">
-                <div className="home__vision-left">
-                    <img className="home__vision-image" src={mission_first} alt="Vision"/>
+        <BrandSlider />
+
+        <div className="home__vision">
+            <div className="home__vision-left">
+                <img className="home__vision-image" src={vision_image} alt="Vision"/>
+            </div>
+                <div className="home__vision-content">
+                    <h2 className="home__vision-title">
+                        Không ngừng <br />
+                        <span>đổi mới và sáng tạo</span>
+                        </h2>
+                    <p className="home__vision-body">
+                        Không chỉ theo kịp xu hướng, chúng tôi còn tạo ra xu hướng. Bởi trong mỗi dự án mà chúng tôi thực hiện đều đề cao sự đầu tư tâm huyết và tinh thần sáng tạo không giới hạn. Hãy để Halago trở thành đối tác đồng hành tin cậy của bạn, cung cấp những giải pháp influencer marketing hiệu quả nhất, thu hút nhất và phù hợp nhất với thị trường hiện tại.
+                    </p>
+                <button className="home__vision-btn">Kết nối ngay với chúng tôi</button>
+            </div>
+        </div>
+
+        <div className="home__strengths">
+            <div className="strengths__left">
+                <h2 className="strengths__left-title"> <span>Thế Mạnh Và Ưu Điểm</span> <br /> Của chúng Tôi</h2>
+                <div className="strengths__left-list">
+                    <details className="list__item">
+                        <summary>Cung cấp đa dạng giải pháp tiếp thị</summary>
+                        <p>Chúng tôi sử dụng sức mạnh của người có ảnh hưởng để mang tới cho nhãn hàng các giải pháp tiếp thị hiệu quả và phù hợp xu hướng như review, livestream, khách mời tham dự workshop, đại sứ thương hiệu, TVC quảng cáo…</p>
+                    </details>
+                    <details className="list__item">
+                        <summary>Chiến lược branding đột phá, sáng tạo</summary>
+                        <p>Từng dự án truyền thông đều được các nhân sự chuyên môn tại Halago sáng tạo thành những câu chuyện mới mẻ với các phương thức tiếp cận mới, thu hút và gây ấn tượng trong tâm trí khách hàng.</p>
+                    </details>
+                    <details className="list__item">
+                        <summary>Mạng lưới influencer đông đảo, chuyên nghiệp</summary>
+                        <p>Hiện tại, Halago đã liên kết và quản lý mạng lưới với hơn 50,000 influencers KOL thuộc nhiều lĩnh vực, trong đó bao gồm cả sự hợp tác với các nghệ sĩ, ca sĩ, diễn viên, hoa hậu, MC...</p>
+                    </details>
                 </div>
-                    <div className="home__vision-content">
-                        <h2 className="home__vision-title">
-                            Không ngừng <br />
-                            <span>đổi mới và sáng tạo</span>
-                            </h2>
-                        <p className="home__vision-body">
-                            Không chỉ theo kịp xu hướng, chúng tôi còn tạo ra xu hướng. Bởi trong mỗi dự án mà chúng tôi thực hiện đều đề cao sự đầu tư tâm huyết và tinh thần sáng tạo không giới hạn. Hãy để Halago trở thành đối tác đồng hành tin cậy của bạn, cung cấp những giải pháp influencer marketing hiệu quả nhất, thu hút nhất và phù hợp nhất với thị trường hiện tại.
-                        </p>
-                    <button className="home__vision-btn">Kết nối ngay với chúng tôi</button>
+                <div className="strengths__left-btn">
+                    <button>Kết nối ngay với chúng tôi</button>
                 </div>
             </div>
 
+            <div className="strengths__right">
+                <img className="strengths__right-image" src={strength_image} alt="Strengths"/>
+            </div>
         </div>
 
     </div>
